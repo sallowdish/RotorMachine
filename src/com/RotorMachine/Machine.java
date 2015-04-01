aaa28b381a14e81753113617dca20db83933bc36
Ray Zheng <zhurd21@gmail.com>
2015-03-31 17:50:10 -0700

    Add Machine class prototype

---
+++ src/com/RotorMachine/Machine.java
@@ -0,0 +1,24 @@
+package com.RotorMachine;
+
+import java.util.ArrayList;
+
+public class Machine {
+
+    Integer secretKey;
+    ArrayList<Rotor> rotors;
+
+    public void intializeMachine(Integer SK){
+        //Load conf
+        //Allocate rotors
+        //Link rotors together
+
+    }
+
+    public String encrypt(String message){
+        return "";
+    }
+
+    public String decrypt(String ciphertext){
+        return "";
+    }
+}
