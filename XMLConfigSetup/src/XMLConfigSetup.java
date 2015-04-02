09c998220fcfa5fc5f2f7199d6b46f8e2a1bcb07
Ray Zheng <zhurd21@gmail.com>
2015-04-01 17:48:00 -0700

    Update

---
+++ XMLConfigSetup/src/XMLConfigSetup.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Properties;
+
+/**
+ * Created by rui.zheng on 01/04/2015
+ */
+public class XMLConfigSetup {
+    public static void main(Stringargs) throws Exception{
+        Properties prop=new Properties();
+        ArrayList<Integer> range= Range.closed(0,127);
+    }
+}
